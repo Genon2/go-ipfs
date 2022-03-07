f5d159794b1e872c887055de9ab423412ba14f0e
Whyros <sebastienpierre1995@gmail.com>
2022-03-07 11:09:25 +0100

    Script updated

--- custom_node.sh
+++ custom_node.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 echo Hello, You are running a script and we need last commit of bitswap to test. Introduce only the last commit from : https://github.com/Genon2/ipfs-thesis-bitswap 
 read input
+if [ "$input" == "No" ] || [ "$input" == "N" ] || [ "$input" == "no" ] || [ "$input" == "n" ]; then
+echo "input is no"
+make build
+cd cmd/ipfs/
+./ipfs daemon
+else
+echo $input
 sed -i 's/BITSWAP/'$input'/g' go.mod
 go mod tidy
 go mod vendor
@@ -10,3 +17,4 @@ sed -i '6d' go.mod
 sed -i '6 i github.com/Genon2/ipfs-thesis-bitswap BITSWAP' go.mod
 cd cmd/ipfs/
 ./ipfs daemon
+fi
